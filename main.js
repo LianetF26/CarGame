4199dfccfa465ff6172ca51a560a9817aaf483f1
LianetF26 <42615906+LianetF26@users.noreply.github.com>
2019-02-20 11:55:55 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -11,33 +11,37 @@ const car = {
         this.mileage += 10;
         alert(`New Mileage: ${this.mileage}`);
     },
-}
-    drivearoundUSA(){
+
+    driveAroundUSA(){
         alert(`Old Mileage: ${this.mileage}`);
         this.mileage += 7000;
         alert(`New Mileage: ${this.mileage}`);
-        alert(`car needs a tuneup!`);
+        alert('Car needs a tuneup!');
+        this.isWorking = false;
     },
+
     getTuneUp(){
-        alert(`Car is fixed and ready to go!`);
+        alert('Car is fixed and ready to go!');
         this.isWorking = true;
     },
 
     honk(){
-        alert(`Honk! Honk!`);
-    };
+        alert("Honk! Honk!");
+    }
+};
 
 // FUNCTIONS
 // Logs all of our car's current stats to the console
-const rewriteStats=()=>{
-    console.log(`Make:${car.make}`);
-    console.log(`Model:${car.model}`);
-    console.log(`Color:${car.color}`);
-    console.log(`Mileage:${car.mileage}`);
-    console.log(`IsWorking:${car.isWorking}`);
-    console.log(``)
+reWriteStats=()=>{
+    console.log(`Make: ${car.make}`);
+    console.log(`Model: ${car.model}`);
+    console.log(`Color: ${car.color}`);
+    console.log(`Mileage: ${car.mileage}`);
+    console.log(`IsWorking: ${car.isWorking}`);
+    console.log('-----------------------------');
 
 }
+
 
 // MAIN PROCESS
 // Captures keyboard input. Depending on the letter pressed it will "call" (execute) different functions.
@@ -47,6 +51,21 @@ document.onkeyup = (event) => {
 
     if (letter === "d") {
         car.driveToWork();
+        reWriteStats();
+    }
+
+    if (letter === "r") {
+        car.driveAroundUSA();
+        reWriteStats();
+    }
+
+    if (letter === "t") {
+        car.getTuneUp();
+        reWriteStats();
+    }
+
+    if (letter === "h") {
+        car.honk();
         reWriteStats();
     }
 
